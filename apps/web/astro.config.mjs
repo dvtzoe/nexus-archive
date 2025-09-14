8426b39171338fe34dc8fb14a5cdf26557151c5a
dvtzoe <ohmtzoe@gmail.com>
2025-09-14 08:53:07 +0700

    redirects / to /home

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -8,8 +8,11 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
 
-  integrations: [react()]
+  integrations: [react()],
+  redirects: {
+    "/": "/home",
+  },
 });
