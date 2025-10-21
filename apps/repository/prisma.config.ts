dc9172cd6a3c2ecd8c48c17662b18350c599e542
dvtzoe <ohmtzoe@gmail.com>
2025-10-21 10:13:37 +0700

    delete prisma config in repository

--- apps/repository/prisma.config.ts
+++
@@ -1,9 +0,0 @@
-import path from "node:path";
-import { defineConfig } from "prisma/config";
-
-export default defineConfig({
-  schema: path.join(__dirname, "prisma", "schema.prisma"),
-  migrations: {
-    seed: path.join(__dirname, "prisma", "seed.ts"),
-  },
-});
